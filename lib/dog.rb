d7783e774157b67714e5f53c7b60800dd9c5848f
Alan Hong <xiupan@gmail.com>
2018-10-15 13:01:37 -0500

    Finished my attempt I think...

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,25 @@
-class Dog
+require 'pry'
+
+connection = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/students.sqlite"
+)
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+class Dog < ActiveRecord::Base
+
 end
+
+# dog = Dog.new({name: "Fido", breed: "lab"})
+
+# binding.pry
+# 0
